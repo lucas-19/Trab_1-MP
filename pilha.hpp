05655cdc7c7ddc501c9cc2d955ba2a38e0a40430
Lucas Costa <lucas.costa@cjr.org.br>
2021-03-06 21:52:04 -0300

    feat: add pilha.hpp

--- pilha.hpp
+++ pilha.hpp
@@ -1,3 +1,43 @@
-int pushPilha(void);
-int popPilha(int);
-int topPilha(void);
+#ifndef PILHA_HPP
+#define PILHA_HPP
+#define CAPACITY 20
+
+#include <istream>
+
+class ValueD {
+    public:
+        int value;
+        ValueD* next;
+};
+
+class ValueS {
+    public:
+        int value;
+};
+
+class StackD {
+    private:
+        ValueD* top;
+        int size;
+
+    public:
+        StackD* createStack();
+        ValueD* push(StackD *stackD, ValueD item);
+        ValueD* pop(StackD *stackD);
+        ValueD* top(StackD *stackD);
+        ValueD* isEmpety(StackD *stackD);
+};
+
+class StackS {
+    private:
+        int size, top, capacity;
+        ValueS stackS[CAPACITY];
+    public:
+        ValueS* push(StackS *stackS, ValueS item);
+        ValueS* pop(StackS *stackS);
+        ValueS* top(StackS *stackS);
+        ValueS* isEmpety(StackS *stackS);
+};
+
+
+#endif // PILHA_HPP
